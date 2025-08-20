757300504e40112ab7e6ecde6212e32117e126da
Ravikant Pandey <ravikant185@gmail.com>
2025-08-20 23:35:31 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/React-Calculator/',  // <-- Add this line
   plugins: [react()],
 })
